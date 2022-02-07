974a1d09fbff49a838758aeb2b0e3afb50791deb
Juan Mitilo <59732631+mitilojuan@users.noreply.github.com>
2022-02-07 10:16:55 -0400

    Add files via upload

---
+++ simple_calculator.py
@@ -0,0 +1,19 @@
+x = input('Enter operator: ')
+y = float(input('Enter first number: '))
+z = float(input('Enter second number: '))
+
+if x == '+':
+	print('Result is: ')
+	print(y+z)
+
+elif x == '-':
+	print('Result is: ')
+	print(y-z)
+
+elif x == '*':
+	print('Result is: ')
+	print(y*z)
+
+elif x == '/':
+	print('Result is: ')
+	print(y/z)
